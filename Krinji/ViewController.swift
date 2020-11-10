86c34d4d5eb6ca2c9cb7c8218e5802075f247227
linooohon <linooohon@gmail.com>
2020-11-10 15:23:46 +0800

    Initial Commit

---
+++ Krinji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Krinji
+//
+//  Created by 林品宏 on 2020/11/10.
+//  Copyright © 2020 pinhunglin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
